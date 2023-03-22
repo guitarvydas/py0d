f5469302fc268344c85ed04076eb2d199761ec09
Paul Tarvydas <guitarvydas@gmail.com>
2023-03-22 09:55:20 -0400

    __init__ for Echo

--- echo.py
+++ echo.py
@@ -1,6 +1,8 @@
 from leaf import Leaf
 
 class Echo (Leaf):
+    def __init__(self,givenName):
+        super().__init__(f'{givenName}/Leaf')
     def handle (self, message):
         self.send (port='stdout', datum=message.datum)
 
--- test.py
+++ test.py
@@ -14,17 +14,18 @@ hw.handle(InputMessage('stdin','Hello'))
 hw.handle(InputMessage('stdin','World'))
 print(hw.outputs())
 
-class WrappedEcho(Echo):
-    def __init__(self):
-        super.__init__('wrapped')
-        self.children = [Echo('echo0'),Echo('echo1')]
-        self.connections = [
-            Down(Sender(None,'stdin'),Receiver(children[0],'stdin')),
-            Across(Sender(children[0],'stdout'),Receiver(children[1],'stdin')),
-            Up(Sender(children[1],'stdout'),Receiver(None,'stdout'))
-            ]
-whw = Echo('whw')
-whw.handle(InputMessage('stdin','wHello'))
-whw.handle(InputMessage('stdin','wWorld'))
-print(whw.outputs())
-            
+# class WrappedEcho(Echo):
+#     def __init__(self):
+#         super.__init__('wrapped')
+#         self.children = [Echo('echo0'),Echo('echo1')]
+#         self.connections = [
+#             Down(Sender(None,'stdin'),Receiver(children[0],'stdin')),
+#             Across(Sender(children[0],'stdout'),Receiver(children[1],'stdin')),
+#             Up(Sender(children[1],'stdout'),Receiver(None,'stdout'))
+#             ]
+# whw = WrappedEcho('whw')
+# whw.handle(InputMessage('stdin','wHello'))
+# whw.handle(InputMessage('stdin','wWorld'))
+# print(whw.outputs())
+
+
